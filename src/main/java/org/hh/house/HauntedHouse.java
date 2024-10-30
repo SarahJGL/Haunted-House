0eaf5bc78d09081754c4498e11594961c76f402f
daniel commins <daniel.commins@techelevator.com>
2024-10-30 14:28:31 -0700

    initial draft

--- src/main/java/org/hh/house/HauntedHouse.java
+++ src/main/java/org/hh/house/HauntedHouse.java
@@ -16,17 +16,17 @@ public class HauntedHouse extends PApplet {
      * TODO: 1. Create an instance variable for a Ghost. Do not initialize it.
      *  Hint: look in the model package for the Ghost class
      */
-    private Ghost ghost;
+
 
     /*
      * TODO: 2. Create an instance variable for a Pumpkin. Do not initialize it.
      */
-    private Pumpkin pumpkin;
+
 
     /*
      * TODO: 3. Create an instance variable for Lightning. Do not initialize it.
      */
-    private Lightning lightning;
+
 
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -40,40 +40,36 @@ public class HauntedHouse extends PApplet {
          * TODO: 4. Assign your ghost instance variable to a new Ghost object
          *  E.g. this.ghost = new Ghost(this);
          */
-        this.ghost = new Ghost(this);
+
 
         /*
          * TODO: 5. Assign your pumpkin instance variable to a new Pumpkin object
          */
-        this.pumpkin = new Pumpkin(this);
-        this.pumpkin.bounce();
+
 
         /*
          * TODO: 6. Assign your lightning instance variable to a new Lightning object
          */
-        this.lightning = new Lightning(this);
+
     }
 
     public void draw() {
-
         drawBackground(this.scaryHouse);
 
         /*
          * TODO: 7. Call the ghost's draw() method
          */
-        ghost.draw();
+
 
         /*
          * TODO: 8. Call the pumpkin's draw() method
          */
-        pumpkin.draw();
+
 
         /*
          * TODO: 9. Call the ghost's draw() method
          */
-        if(mousePressed){
-            lightning.draw();
-        }
+
 
         /*
          * TODO: 10. Only draw lightning if the mouseIsPressed
